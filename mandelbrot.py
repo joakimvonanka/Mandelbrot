2a17fd55a6a41a0b3f77aeb806d358e671f33731
Joakim von Anka <53865379+joakimvonanka@users.noreply.github.com>
2024-06-07 10:51:46 +0100

    Updated

--- mandelbrot.py
+++ mandelbrot.py
@@ -3,7 +3,6 @@ from PIL import Image
 from tqdm import tqdm
 from numba import jit, prange
 
-
 def mandelbrot(c, max_iter):
     z = np.zeros(c.shape, dtype=np.complex128)
     divtime = max_iter + np.zeros(c.shape, dtype=np.int32)
@@ -16,28 +15,32 @@ def mandelbrot(c, max_iter):
 
     return divtime
 
-def create_mandelbrot_image(width, height, x_centre, y_centre, zoom, max_iter, output_filename):
-    image = Image.new("RGB", (width, height))
-    pixels = image.load()
+def create_mandelbrot_image(width, height, x_min, x_max, y_min, y_max, max_iter, output_filename):
+    x = np.linspace(x_min, x_max, width)
+    y = np.linspace(y_min, y_max, height)
+    X, Y = np.meshgrid(x, y)
+    C = X + 1j * Y
 
-    for x in tqdm(range(width), desc="Generating Mandelbrot", unit="pixel"):
-        for y in range(height):
-            re = x_min + (x / width) * (x_max - x_min)
-            im = y_min + (y / height) * (y_max - y_min)
-            c = complex(re, im)
-            colour_value = mandelbrot(c, max_iter)
-            colour = 255 - int(colour_value * 255 / max_iter)
-            pixels[x, y] = (colour, colour, colour)
+    divtime = np.zeros(C.shape, dtype=np.int32)
+    chunk_size = 100
 
+    for i in tqdm(range(0, width, chunk_size), desc="Generating Mandelbrot"):
+        end = min(i + chunk_size, width)
+        divtime_chunk = mandelbrot(C[i:end], max_iter)
+        divtime[i:end] = divtime_chunk
+
+    divtime = 255 - (divtime * 255 / max_iter).astype(np.uint8)
+
+    image = Image.fromarray(divtime)
     image.save(output_filename)
 
-width = 8000
-height = 8000
+width = 16000
+height = 16000
 x_min = -2.0
 x_max = 1.0
 y_min = -1.5
 y_max = 1.5
-max_iter = 256
+max_iter = 1000
 output_filename = "high_res_mandelbrot.png"
 
 create_mandelbrot_image(width, height, x_min, x_max, y_min, y_max, max_iter, output_filename)
